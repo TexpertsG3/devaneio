0f505cdfc7e564a9b4e1e72f7421172c7d0f4d92
Albino Marques <albinomarques01@gmail.com>
2023-03-22 23:28:28 -0300

    Feito o início do projeto Spring do Back-end do Hotel Devaneio.

---
+++ src/main/java/br/com/hotel/devaneio/DevaneioApplication.java
@@ -0,0 +1,13 @@
+package br.com.hotel.devaneio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevaneioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevaneioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/hotel/devaneio/controller/HomeController.java
@@ -0,0 +1,42 @@
+package br.com.hotel.devaneio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    //Exemplo de modelos -  Banco: ContaCorrente, Cliente, Agência, Banco, Funcionarios, ClienteDAO, ClienteRepository,
+    // ClienteService
+
+    @GetMapping
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        String teste = "Teste de conexão";
+
+        modelAndView.addObject("home", teste);
+
+        return modelAndView;
+    }
+
+//    @PostMapping("/um")
+//    public String cadastraProdutos(){
+//        System.out.println("Página Home");
+//        return "home";
+//    }
+//
+//    @PostMapping("/dois") //Aqui só acessa com o POST
+//    public String cadastraProdutor(){
+//        System.out.println("Página home2");
+//        return "home2";
+//    }
+}
+
